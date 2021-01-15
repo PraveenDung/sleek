0ab1bd1be9cfb15721f42625e9993a86bd976adc
PraveenDung <51016137+PraveenDung@users.noreply.github.com>
2021-01-15 19:32:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,21 +10,22 @@ def respond():
     #print(request.json);
     ticket_detail = request.json
     
-    if(ticket_detail[0]['payload']['firstThread']['author']['name'].lower()=='qualyval test'):
-        with open('log.csv', mode='a+', newline='') as log:
-            log_writer = csv.writer(log, delimiter=',')
-            try:
-                ticket_no = str(ticket_detail[0]['payload']['ticketNumber'])
-                invoice_no = main_process(ticket_detail)
-                in_arr = [ticket_no,invoice_no,'Success','None']
-                log_writer.writerow(in_arr)
-            except Exception as e:
+    if(len(ticket_detail)!=0):
+        if(ticket_detail[0]['payload']['firstThread']['author']['name'].lower()=='qualyval test'):
+            with open('log.csv', mode='a+', newline='') as log:
+                log_writer = csv.writer(log, delimiter=',')
                 try:
                     ticket_no = str(ticket_detail[0]['payload']['ticketNumber'])
-                    in_arr = [ticket_no,'','Fail',str(e)]
+                    invoice_no = main_process(ticket_detail)
+                    in_arr = [ticket_no,invoice_no,'Success','None']
                     log_writer.writerow(in_arr)
-                except:
-                    pass    
+                except Exception as e:
+                    try:
+                        ticket_no = str(ticket_detail[0]['payload']['ticketNumber'])
+                        in_arr = [ticket_no,'','Fail',str(e)]
+                        log_writer.writerow(in_arr)
+                    except:
+                        pass    
     return Response(status=200)
 
 if __name__=='__main__':
